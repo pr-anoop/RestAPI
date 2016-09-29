33447e66456bd06a86a400d0563cf60cd1a42c8e
Anoop P R <itsanoop@outlook.com>
2016-09-29 12:08:29 +0530

    Initial setup

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var port = 8080;
+// var book = require('./app/routes/book');
+var config = require('config'); //we load the db location from the JSON files
+//db options
+var options = {
+    server: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    }
+};
+
+//db connection      
+mongoose.connect(config.DBHost, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//don't show the log when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
+
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: 'application/json'
+}));
+
+app.get("/", (req, res) => res.json({
+    message: "Welcome to our Bookstore!"
+}));
+
+/*app.route("/book")
+    .get(book.getBooks)
+    .post(book.postBook);
+app.route("/book/:id")
+    .get(book.getBook)
+    .delete(book.deleteBook)
+    .put(book.updateBook);*/
+
+app.listen(port);
+console.log("Listening on port " + port);
+
+module.exports = app;
